2602f0456223f40d4f5bf6482b67f47940d629c3
Vera Christ <vchrist@uni-koblenz.de>
2019-07-11 16:57:20 +0200

    vid

--- vid.py
+++ vid.py
@@ -4,22 +4,33 @@ import PIL.Image, PIL.ImageTk
 import time
  
 class App:
-	def __init__(self, window, window_title, video_source=0):
+	def __init__(self, window, window_title, video_source1=0, video_source2=0):
 		self.window = window
 		self.window.title(window_title)
-		self.video_source = video_source
+		self.video_source1 = video_source1
+		self.video_source2 = video_source2
+
  
-		# open video source (by default this will try to open the computer webcam)
-		self.vid = MyVideoCapture(self.video_source)
+		# open video sources (by default this will try to open the computer webcam)
+		self.vid1 = MyVideoCapture(self.video_source1)
+		self.vid2 = MyVideoCapture(self.video_source2)
+
  
 		# Create a canvas that can fit the above video source size
-		self.canvas = tkinter.Canvas(window, width = self.vid.width, height = self.vid.height)
-		self.canvas.pack()
+		self.canvas1 = tkinter.Canvas(window, width = self.vid1.width, height = self.vid1.height)
+		self.canvas1.pack(side="left", fill="both", expand= True)
+
+		self.canvas2 = tkinter.Canvas(window, width = self.vid2.width, height = self.vid2.height)
+		self.canvas2.pack(side="right", fill="both", expand= False)
  
+ 		# Calculate maximal hight
+ 		self.maxHeight = self.vid1.height if (self.vid1.height > self.vid2.height) else self.vid2.height
 		# Button that lets the user take a snapshot
 		self.btn_snapshot=tkinter.Button(window, text="Snapshot", width=50, command=self.snapshot)
-		self.btn_snapshot.pack(anchor=tkinter.CENTER, expand=True)
- 
+		self.btn_snapshot.pack(fill="both", expand=True)
+		#self.btn_snapshot.place(x= 0, y=self.maxHeight, width= 200, height=30)
+		self.btn_snapshot.place(x = 0, y = 0, relwidth = 0.1, relheight = 0.1)
+
 		# After it is called once, the update method will be automatically called every delay milliseconds
 		self.delay = 15
 		self.update()
@@ -28,18 +39,29 @@ class App:
  
 	def snapshot(self):
 		# Get a frame from the video source
-		ret, frame = self.vid.get_frame()
+		ret, frame = self.vid1.get_frame()
  
 		if ret:
 			cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
  
 	def update(self):
 		# Get a frame from the video source
-		ret, frame = self.vid.get_frame()
+		ret1, frame1 = self.vid1.get_frame()
+		ret2, frame2 = self.vid2.get_frame()
  
-		if ret:
-			self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
-			self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
+		if ret1:
+			self.photo1 = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame1))
+			self.canvas1.create_image(0, 0, image = self.photo1, anchor = tkinter.NW)
+		if ret2:
+			self.photo2 = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame2))
+			self.canvas2.create_image(0, 0, image = self.photo2, anchor = tkinter.NW)
+
+		if ret1 & ret2:
+			self.photo1 = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame1))
+			self.photo2 = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame2))
+			self.canvas1.create_image(0, 0, image = self.photo1, anchor = tkinter.NW)
+			self.canvas2.create_image(0, 0, image = self.photo2, anchor = tkinter.NW)
+
  
 		self.window.after(self.delay, self.update)
  
@@ -72,4 +94,4 @@ class MyVideoCapture:
 			self.vid.release()
  
 # Create a window and pass it to the Application object
-App(tkinter.Tk(), "Tkinter and OpenCV")
+App(tkinter.Tk(), "Video Analyser/Comparer", 0, 'a1QnG8Y_460svvp9.webm')
