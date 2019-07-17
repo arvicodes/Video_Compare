9b7a85d64b87c1eecc1f7827a3f107c562e3b521
Vera Christ <vchrist@uni-koblenz.de>
2019-07-17 17:18:48 +0200

    2

--- vid.py
+++ vid.py
@@ -19,34 +19,39 @@ class App:
 
  
 		# open video sources (by default this will try to open the computer webcam)
-		self.filename = tkFileDialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))		
-		self.vid1 = MyVideoCapture(self.video_source1)
-		self.vid2 = MyVideoCapture(self.video_source2)
+		self.vid1 = None
+		self.vid2 = None
+		#self.vid1 = MyVideoCapture(self.video_source1)
+		#self.vid2 = MyVideoCapture(self.video_source2)
 
  
+		# Button for opening File on left & right side
+		self.button_left = Button(text="Open first video", command=self.callback_openFirstFile)
+		self.button_right = Button(text="Open second video", command=self.callback_openSecondFile)
+
+
 		# Create a canvas that can fit the above video source size
-		self.canvas1 = tkinter.Canvas(window, width = self.vid1.width, height = self.vid1.height)
+		self.canvas1 = tkinter.Canvas(window, width = 400, height = 400)
 		self.canvas1.pack(side="left", fill="both", expand= True)
 		self.canvas1.configure(bg="white")
 		self.m1.add(self.canvas1)
-
-		self.openFile1 = Button(text="Open first video", command=callback_openFirstFile)
-		self.m1.add(self.openFile1)
+		self.m1.add(self.button_left)
 
 
-		self.canvas2 = tkinter.Canvas(window, width = self.vid2.width, height = self.vid2.height)
+		self.canvas2 = tkinter.Canvas(window, width = 400, height = 400)
 		self.canvas2.pack(side="left", fill="both", expand= False)
 		self.canvas2.configure(bg="white")
 		self.m1.add(self.canvas2)
+		self.m1.add(self.button_right)
 
  
  		# Calculate maximal hight
- 		self.maxHeight = self.vid1.height if (self.vid1.height > self.vid2.height) else self.vid2.height
+ 		##self.maxHeight = self.vid1.height if (self.vid1.height > self.vid2.height) else self.vid2.height
 		# Button that lets the user take a snapshot
-		self.btn_snapshot=tkinter.Button(window, text="Snapshot", width=50, command=self.snapshot)
-		self.btn_snapshot.pack(fill="both", expand=True)
+		##self.btn_snapshot=tkinter.Button(window, text="Snapshot", width=50, command=self.snapshot)
+		##self.btn_snapshot.pack(fill="both", expand=True)
 		#self.btn_snapshot.place(x= 0, y=self.maxHeight, width= 200, height=30)
-		self.btn_snapshot.place(x = 0, y = 0, relwidth = 0.1, relheight = 0.1)
+		##self.btn_snapshot.place(x = 0, y = 0, relwidth = 0.1, relheight = 0.1)
 
 		# After it is called once, the update method will be automatically called every delay milliseconds
 		self.delay = 15
@@ -63,27 +68,39 @@ class App:
  
 	def update(self):
 
-		self.window.geometry('{}x{}'.format(int(self.vid1.width+self.vid2.width), int(self.maxHeight)))
+		#self.window.geometry('{}x{}'.format(int(self.vid1.width+self.vid2.width), int(self.maxHeight)))
 		# Get a frame from the video source
-		ret1, frame1 = self.vid1.get_frame()
-		ret2, frame2 = self.vid2.get_frame()
- 
-		if ret1:
-			self.photo1 = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame1))
-			self.canvas1.create_image(0, 0, image = self.photo1, anchor = tkinter.NW)
-		if ret2:
-			self.photo2 = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame2))
-			self.canvas2.create_image(0, 0, image = self.photo2, anchor = tkinter.NW)
+		if self.vid1 != None:
+			ret1, frame1 = self.vid1.get_frame()
 
-		if ret1 & ret2:
-			self.photo1 = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame1))
-			self.photo2 = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame2))
-			self.canvas1.create_image(0, 0, image = self.photo1, anchor = tkinter.NW)
-			self.canvas2.create_image(0, 0, image = self.photo2, anchor = tkinter.NW)
+			if ret1:
+				self.photo1 = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame1))
+				self.canvas1.create_image(0, 0, image = self.photo1, anchor = tkinter.NW)
+ 
+ 		if self.vid2 != None:
+ 			ret2, frame2 = self.vid2.get_frame()
+
+			if ret2:
+				self.photo2 = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame2))
+				self.canvas2.create_image(0, 0, image = self.photo2, anchor = tkinter.NW)
+
+			#if ret1 & ret2:
+				#self.photo1 = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame1))
+				#self.photo2 = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame2))
+				#self.canvas1.create_image(0, 0, image = self.photo1, anchor = tkinter.NW)
+				#self.canvas2.create_image(0, 0, image = self.photo2, anchor = tkinter.NW)
 
  
 		self.window.after(self.delay, self.update)
- 
+
+
+	def callback_openFirstFile(self):
+		self.video_source1 = tkFileDialog.askopenfilename(title = "Select file", filetypes = (("all files","*.*"),("avi files","*.avi"), ("mp4 files","*.mp4"),("webm files","*.webm")))		
+		self.vid1 = MyVideoCapture(self.video_source1)
+
+ 	def callback_openSecondFile(self):
+		self.video_source2 = tkFileDialog.askopenfilename(title = "Select file", filetypes = (("all files","*.*"),("avi files","*.avi"), ("mp4 files","*.mp4"),("webm files","*.webm")))		
+		self.vid2 = MyVideoCapture(self.video_source2)
  
 class MyVideoCapture:
 	def __init__(self, video_source=0):
